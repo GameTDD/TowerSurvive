a286d02831181c5fa40ade4366224c58c43684b5
Julia Naomi <jnboeira@outlook.com>
2021-08-02 01:55:01 -0300

    tank rotation mechanic

--- src/objects.rs
+++ src/objects.rs
@@ -4,28 +4,32 @@ use ggez;
 use ggez::event;
 use ggez::event::KeyCode;
 use ggez::graphics;
-use ggez::graphics::Color;
 use ggez::nalgebra as na;
+use ggez::timer::delta;
 
 pub struct Tank {
     pub position: na::Point2<f32>,
-    pub direction: na::Vector2<f32>,
+    pub tank_direction: na::Vector2<f32>,
+    pub tank_rotation: f32,
     pub texture: Option<graphics::Image>,
 }
 
 impl event::EventHandler for Tank {
     fn update(&mut self, ctx: &mut ggez::Context) -> ggez::GameResult {
         let keys = ggez::input::keyboard::pressed_keys(ctx);
+        let delta = delta(ctx).as_secs_f32();
         self.movement(keys);
+        self.rotation(keys, delta);
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut ggez::Context) -> ggez::GameResult {
-        graphics::draw(
-            ctx,
-            self.texture.as_ref().unwrap(),
-            (self.position, 0.0, Color::from_rgb(255, 255, 255)),
-        )?;
+        let param = graphics::DrawParam::new()
+            .dest(self.position)
+            .offset(na::Point2::from([0.5, 0.5]))
+            .rotation(self.tank_rotation);
+
+        graphics::draw(ctx, self.texture.as_ref().unwrap(), param)?;
         Ok(())
     }
 }
@@ -33,18 +37,36 @@ impl event::EventHandler for Tank {
 impl Tank {
     pub fn movement(&mut self, keys: &HashSet<KeyCode>) {
         if keys.contains(&KeyCode::W) || keys.contains(&KeyCode::Up) {
-            self.position = na::Point2::from([
-                self.position.x + self.direction.x,
-                self.position.y + self.direction.y,
-            ]);
+            self.update_position(1.);
         }
 
         if keys.contains(&KeyCode::S) || keys.contains(&KeyCode::Down) {
-            self.position = na::Point2::from([
-                self.position.x - self.direction.x,
-                self.position.y - self.direction.y,
-            ]);
+            self.update_position(-1.);
         }
+    }
+
+    fn rotation(&mut self, keys: &HashSet<KeyCode>, delta: f32) {
+        if keys.contains(&KeyCode::D) || keys.contains(&KeyCode::Right) {
+            self.tank_rotation += delta;
+            self.update_direction();
+        }
+
+        if keys.contains(&KeyCode::A) || keys.contains(&KeyCode::Left) {
+            self.tank_rotation -= delta;
+            self.update_direction();
+        }
+    }
+
+    fn update_direction(&mut self) {
+        let (sin, cos) = self.tank_rotation.sin_cos();
+        self.tank_direction = na::Vector2::from([-cos, -sin]);
+    }
+
+    fn update_position(&mut self, direction: f32) {
+        self.position = na::Point2::from([
+            self.position.x + (direction * self.tank_direction.x),
+            self.position.y + (direction * self.tank_direction.y),
+        ]);
     }
 }
 
@@ -56,7 +78,7 @@ mod test {
     fn initial_info() {
         let tank = tank();
         assert_eq!(tank.position, na::Point2::from([400., 300.]));
-        assert_eq!(tank.direction, na::Vector2::from([-1., 0.]));
+        assert_eq!(tank.tank_direction, na::Vector2::from([-1., 0.]));
     }
 
     #[test]
@@ -81,10 +103,43 @@ mod test {
         assert_eq!(tank.position, na::Point2::from([402., 300.]));
     }
 
+    #[test]
+    fn turn_left() {
+        let mut tank = tank();
+        let keys = vec![KeyCode::A].into_iter().collect();
+        tank.rotation(&keys, 0.3);
+        let keys = vec![KeyCode::W].into_iter().collect();
+        tank.movement(&keys);
+        assert_eq!(tank.position, na::Point2::from([399.04468, 300.29553]));
+
+        let keys = vec![KeyCode::Left].into_iter().collect();
+        tank.rotation(&keys, 0.5);
+        let keys = vec![KeyCode::W].into_iter().collect();
+        tank.movement(&keys);
+        assert_eq!(tank.position, na::Point2::from([398.34796, 301.01288]));
+    }
+
+    #[test]
+    fn turn_right() {
+        let mut tank = tank();
+        let keys = vec![KeyCode::D].into_iter().collect();
+        tank.rotation(&keys, 0.3);
+        let keys = vec![KeyCode::S].into_iter().collect();
+        tank.movement(&keys);
+        assert_eq!(tank.position, na::Point2::from([400.95532, 300.29553]));
+
+        let keys = vec![KeyCode::Right].into_iter().collect();
+        tank.rotation(&keys, 0.5);
+        let keys = vec![KeyCode::S].into_iter().collect();
+        tank.movement(&keys);
+        assert_eq!(tank.position, na::Point2::from([401.65204, 301.01288]));
+    }
+
     fn tank() -> Tank {
         Tank {
             position: na::Point2::from([400., 300.]),
-            direction: na::Vector2::from([-1., 0.]),
+            tank_direction: na::Vector2::from([-1., 0.]),
+            tank_rotation: 0.,
             texture: None,
         }
     }
